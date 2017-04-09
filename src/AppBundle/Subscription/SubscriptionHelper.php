60cde8e7519cc3985ed25d85c2c1bbc03c62afea
petrero <petrero@gmail.com>
2017-04-09 15:28:21 +0300

    Since our goal is to update the database during checkout, go back to OrderController and find the chargeCustomer() method that holds all the magic.
    But instead of putting the code to update the database right here, let's add it to SubscriptionHelper: this class will do all the work related to subscriptions. Add a new method at the bottom called public function addSubscriptionToUser() with two arguments: the \Stripe\Subscription object that was just created and the User that the Subscription should belong to.
    Inside, start with {dollar_sign}subscription = {dollar_sign}user->getSubscription(). So, the user may already have a row in the subscription table from a previous, expired subscription. If they do, we'll just update that row instead of creating a second row. Every User will have a maximum of one related row in the subscription table. It keeps things simple.
    But if they don't have a previous subscription, let's create one: {dollar_sign}subscription = new Subscription(). Then, {dollar_sign}subscription->setUser({dollar_sign}user)

--- src/AppBundle/Subscription/SubscriptionHelper.php
+++ src/AppBundle/Subscription/SubscriptionHelper.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Subscription;
 
+use AppBundle\Entity\User;
+use AppBundle\Entity\Subscription;
+
 class SubscriptionHelper {
   /** @var SubscriptionPlan[] */
   private $plans = [];
@@ -29,6 +32,14 @@ class SubscriptionHelper {
       if ($plan->getPlanId() == $planId) {
         return $plan;
       }
+    }
+  }
+  
+  public function addSubscriptionToUser(\Stripe\Subscription $stripeSubscription, User $user) {
+    $subscription = $user->getSubscription();
+    if (!$subscription) {
+      $subscription = new Subscription();
+      $subscription->setUser($user);
     }
   }
 }
