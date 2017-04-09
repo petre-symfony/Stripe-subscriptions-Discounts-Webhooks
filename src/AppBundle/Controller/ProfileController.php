9cd7e88ac28d6b0edba5cdd2ef4886532ae4f805
petrero <petrero@gmail.com>
2017-04-09 18:52:21 +0300

    Copy code from branch 7

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Security("is_granted('ROLE_USER')")
@@ -14,5 +15,17 @@ class ProfileController extends BaseController {
    */
   public function accountAction() {
     return $this->render('profile/account.html.twig');
+  }
+  /**
+   * @Route("/profile/subscription/cancel", name="account_subscription_cancel")
+   * @Method("POST")
+   */
+  public function cancelSubscriptionAction() {
+    $stripeClient = $this->get('stripe_client'); 
+    $stripeClient->cancelSubscription($this->getUser());
+    
+    $this->addFlash('success', 'Subscription Canceled :(');
+    
+    return $this->redirectToRoute('profile_account');
   }
 }
