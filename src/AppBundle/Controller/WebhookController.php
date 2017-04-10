b98174ff53e4ceffb8aa6819c03de3911865cd04
petrero <petrero@gmail.com>
2017-04-10 21:42:19 +0300

    Let's get right to work on our webhook endpoint. In the src/AppBundle/Controller directory, create a new WebhookController class. Make it extend a BaseController class I created - that just has a few small shortcuts.
    Now, create the endpoint with public function stripeWebhookAction(). Give an @Route annotation set to /webhooks/stripe and a similar name. Make sure you have the Route use statement.
    Start simple: return a new Response() from the HttpFoundation component.
    That's just enough to try it out: find your browser and go to /webhooks/stripe. It's alive!

---
+++ src/AppBundle/Controller/WebhookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class WebhookController extends BaseController{
+  /** 
+   * @Route("/webhooks/stripe", name="webhook_stripe")
+   */  
+  public function stripeWebhookAction(Request $request){
+    return new Response('baaaa');  
+  }
+}
