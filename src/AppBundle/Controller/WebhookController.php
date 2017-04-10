8c8a08633f24f1afc97bcf82f71552a264379886
petrero <petrero@gmail.com>
2017-04-10 21:49:53 +0300

    Thanks to RequestBin, we know more or less what the JSON body will look like. The most important thing is this event id. Let's decode the JSON and grab this.
    To do that, add {dollar_sign}data = json_decode(), but pause there. We need to pass this the body of the Request. In Symfony, we get this by adding a Request argument - don't forget the use statement! Then, use {dollar_sign}request->getContent(). Also, pass true as the second argument so that json_decode returns an associative array.
    Next, it shouldn't happen, but just in case, if {dollar_sign}data is null, that means Stripe sent us invalid JSON. Shame on you Stripe! Throw an exception in this case... and make sure you spell Exception correctly!
    Finally, get the {dollar_sign}eventId from {dollar_sign}data['id']

--- src/AppBundle/Controller/WebhookController.php
+++ src/AppBundle/Controller/WebhookController.php
@@ -11,6 +11,14 @@ class WebhookController extends BaseController{
    * @Route("/webhooks/stripe", name="webhook_stripe")
    */  
   public function stripeWebhookAction(Request $request){
+    $data = json_decode($request->getContent(), true);
+    
+    if ($data == null){
+      throw new Exception("Bad JSON body from Stripe!");
+    }
+    
+    $eventId = $data['id'];
+    
     return new Response('baaaa');  
   }
 }
