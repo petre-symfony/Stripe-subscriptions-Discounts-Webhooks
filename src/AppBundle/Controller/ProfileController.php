a20a6980a301bdcea9f74054db3054bd19457692
petrero <petrero@gmail.com>
2017-04-10 18:20:59 +0300

    Copy code from branch 10

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Security("is_granted('ROLE_USER')")
@@ -14,5 +15,42 @@ class ProfileController extends BaseController {
    */
   public function accountAction() {
     return $this->render('profile/account.html.twig');
+  }
+  /**
+   * @Route("/profile/subscription/cancel", name="account_subscription_cancel")
+   * @Method("POST")
+   */
+  public function cancelSubscriptionAction() {
+    $stripeClient = $this->get('stripe_client'); 
+    $stripeSubscription = $stripeClient->cancelSubscription($this->getUser());
+    
+    $subscription = $this->getUser()->getSubscription();
+    if ($stripeSubscription->status == 'canceled'){
+      $subscription->cancel();
+    } else {
+      $subscription->deactivateSubscription();
+    }
+    
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($subscription);
+    $em->flush();
+    
+    $this->addFlash('success', 'Subscription Canceled :(');
+    
+    return $this->redirectToRoute('profile_account');
+  }
+  
+  /**
+   * @Route("/profile/subscription/reactivate", name="account_subscription_reactivate")
+   */
+  public function reactivateSubscriptionAction() { 
+    $stripeClient = $this->get('stripe_client');
+    $stripeSubscription = $stripeClient->reactivateSubscription($this->getUser());
+    
+    $this->get('subscription_helper')->addSubscriptionToUser($stripeSubscription, $this->getUser());
+    
+    $this->addFlash('success', 'Welcome back!');
+    
+    return $this->redirectToRoute('profile_account');
   }
 }
