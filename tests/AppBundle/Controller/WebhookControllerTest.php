8fffa6d37c61957853f9bddca9a1d36869cc3c4a
petrero <petrero@gmail.com>
2017-04-11 19:50:53 +0300

    To send the webhook, we first need to prepare a JSON string that matches what Stripe sends. At the bottom of the class, create a new private function called getCustomerSubscriptionDeletedEvent() with a {dollar_sign}subscriptionId argument.
    To fill this in, go copy the real JSON from the test webhook. Paste it here with {dollar_sign}json = <<<EOF enter, and paste!
    
    Now here's the important part: our controller reads the data.object.id key to find the subscription id. Replace this with %s.
    
    Then, finish the function with return sprintf({dollar_sign}json, {dollar_sign}subscriptionId).
    
    Now, this function will create a realistic-looking JSON string, but with whatever subscriptionId we want!
    
    Back in the test function, add {dollar_sign}eventJson = {dollar_sign}this->getCustomerSubscriptionDeletedEvent() and pass it {dollar_sign}subscription->getStripeSubscriptionId(), which is some fake, random value that the function below created

--- tests/AppBundle/Controller/WebhookControllerTest.php
+++ tests/AppBundle/Controller/WebhookControllerTest.php
@@ -21,7 +21,9 @@ class WebhookControllerTest extends WebTestCase {
   public function testStripeCustomerSubscriptionDeleted() {
     $subscription = $this->createSubscription();
     
-    //todo send the cancelation webhook
+    $eventJson = $this->getCustomerSubscriptionDeletedEvent(
+      $subscription->getStripeSubscriptionId()      
+    );
     
     $this->assertFalse($subscription->isActive());
   }
@@ -45,5 +47,61 @@ class WebhookControllerTest extends WebTestCase {
     $this->em->flush();
 
     return $subscription;
+  }
+  
+  private function getCustomerSubscriptionDeletedEvent($subscriptionId) {
+    $json = <<<EOF 
+{
+  "created": 1326853478,
+  "livemode": false,
+  "id": "evt_00000000000000",
+  "type": "customer.subscription.deleted",
+  "object": "event",
+  "request": null,
+  "pending_webhooks": 1,
+  "api_version": "2016-07-06",
+  "data": {
+    "object": {
+      "id": "%s",
+      "object": "subscription",
+      "application_fee_percent": null,
+      "cancel_at_period_end": true,
+      "canceled_at": 1469731697,
+      "created": 1469729305,
+      "current_period_end": 1472407705,
+      "current_period_start": 1469729305,
+      "customer": "cus_00000000000000",
+      "discount": null,
+      "ended_at": 1470436151,
+      "livemode": false,
+      "metadata": {
+      },
+      "plan": {
+        "id": "farmer_00000000000000",
+        "object": "plan",
+        "amount": 9900,
+        "created": 1469720306,
+        "currency": "usd",
+        "interval": "month",
+        "interval_count": 1,
+        "livemode": false,
+        "metadata": {
+        },
+        "name": "Farmer Brent (monthly)",
+        "statement_descriptor": null,
+        "trial_period_days": null
+      },
+      "quantity": 1,
+      "start": 1469729305,
+      "status": "canceled",
+      "tax_percent": null,
+      "trial_end": null,
+      "trial_start": null
+    }
+  }
+}            
+EOF;
+  
+    return sprintf($json, $subscriptionId);        
   }
 }
