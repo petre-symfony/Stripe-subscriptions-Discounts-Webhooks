c68ba9f0657735b8278452ed2d562248c1a46d25
petrero <petrero@gmail.com>
2017-04-11 18:52:35 +0300

    Copy code from branch 16

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
 use AppBundle\Entity\User;
+use AppBundle\Store\ShoppingCart;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -27,7 +28,16 @@ class OrderController extends BaseController {
    * @Route("/cart/subscription/{planId}", name="order_add_subscription_to_cart")
    */
   public function addSubscriptionToCartAction($planId) {
-    // todo - add the subscription plan to the cart!
+    $subscriptionHelper = $this->get('subscription_helper');
+    $plan = $subscriptionHelper->findPlan($planId);
+    
+    if(!$plan) {
+      throw $this->createNotFoundException('Bad plan id!');
+    }
+    
+    $this->get('shopping_cart')->addSubscription($planId);
+    
+    return $this->redirectToRoute('order_checkout');
   }
 
   /**
@@ -73,11 +83,15 @@ class OrderController extends BaseController {
     /** @var User $user */
     $user = $this->getUser();
     if (!$user->getStripeCustomerId()) {
-      $stripeClient->createCustomer($user, $token);
+      $stripeCustomer = $stripeClient->createCustomer($user, $token);
     } else {
-      $stripeClient->updateCustomerCard($user, $token);
+      $stripeCustomer = $stripeClient->updateCustomerCard($user, $token);
     }
+    
+    //save card details
+    $this->get('subscription_helper')->updateCardDetails($user, $stripeCustomer);
 
+    /** @var ShoppingCart $cart */
     $cart = $this->get('shopping_cart');
 
     foreach ($cart->getProducts() as $product) {
@@ -87,7 +101,17 @@ class OrderController extends BaseController {
         $product->getName()
       );
     }
-    $stripeClient->createInvoice($user, true);
+    
+    if($cart->getSubscriptionPlan()){
+      // a subscription creates an invoice
+      $stripeSubscription = $stripeClient->createSubscription($user, $cart->getSubscriptionPlan());
+      
+      $this->get('subscription_helper')->addSubscriptionToUser($stripeSubscription, $user);
+    } else {
+      //charge the invoice!
+      $stripeClient->createInvoice($user, true);
+    }
+    
   }
 }
 
---
+++ src/AppBundle/Controller/WebhookController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class WebhookController extends BaseController{
+  /** 
+   * @Route("/webhooks/stripe", name="webhook_stripe")
+   */  
+  public function stripeWebhookAction(Request $request){
+    $data = json_decode($request->getContent(), true);
+    
+    if ($data == null){
+      throw new Exception("Bad JSON body from Stripe!");
+    }
+    
+    $eventId = $data['id'];
+    $stripeEvent = $this->get('stripe_client')->findEvent($eventId);
+    
+    $subscriptionHelper = $this->get('subscription_helper');
+    switch ($stripeEvent->type){
+      case 'customer.subscription.deleted':
+        $stripeSubscriptionId = $stripeEvent->data->object->id;
+        $subscription = $this->findSubscription($stripeSubscriptionId);
+        
+        $subscriptionHelper->fullyCancelSubscription($subscription);
+        break;
+      default: 
+        throw new \Exception('Unexpected webhook type form Stripe! '.$stripeEvent->type);
+    }
+    
+    return new Response('Event handled: ' . $stripeEvent->type);  
+  }
+  
+  /**
+   * @param $stripeSubscriptionId
+   * @return \AppBundle\Entity\Subscription
+   * @throws \Exception
+   */
+  private function  findSubscription($stripeSubscriptionId){
+    $subscription = $this->getDoctrine()
+      ->getRepository('AppBundle:Subscription')
+      ->findOneBy([
+          'stripeSubscriptionId' => $stripeSubscriptionId 
+      ]); 
+    
+    if (!$subscription) {
+      throw new \Exception('Somehow we have no subscription id ' . $stripeSubscriptionId);
+    }
+    
+    return $subscription;
+  }
+}
