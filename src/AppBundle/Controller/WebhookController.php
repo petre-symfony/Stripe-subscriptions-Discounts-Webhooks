05bc8744d8588612af19013cf92f9e84dd1804cb
petrero <petrero@gmail.com>
2017-04-14 21:34:17 +0300

    Copy code from branch 34

---
+++ src/AppBundle/Controller/WebhookController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\StripeEventLog;
+
+class WebhookController extends BaseController{
+  /** 
+   * @Route("/webhooks/stripe", name="webhook_stripe")
+   */  
+  public function stripeWebhookAction(Request $request){
+    $data = json_decode($request->getContent(), true);
+    
+    if ($data == null){
+      throw new Exception("Bad JSON body from Stripe!");
+    }
+    
+    $eventId = $data['id'];
+    
+    $em = $this->getDoctrine()->getManager();
+    $existingLog = $em->getRepository('AppBundle:StripeEventLog')
+      ->findOneBy(['stripeEventId' => $eventId]);
+    if ($existingLog) {
+      return new Response('Event previously handled');
+    }
+    
+    $log = new StripeEventLog($eventId);
+    $em->persist($log);
+    $em->flush($log);
+    
+    if ($this->getParameter('verify_stripe_event')){
+      $stripeEvent = $this->get('stripe_client')->findEvent($eventId);
+    } else {
+      //fake the stripe_Event in the test environment
+      $stripeEvent = json_decode($request->getContent());
+    }
+    
+    
+    $subscriptionHelper = $this->get('subscription_helper');
+    switch ($stripeEvent->type){
+      case 'customer.subscription.deleted':
+        $stripeSubscriptionId = $stripeEvent->data->object->id;
+        $subscription = $this->findSubscription($stripeSubscriptionId);
+        
+        $subscriptionHelper->fullyCancelSubscription($subscription);
+        break;
+      case 'invoice.payment_succeeded':
+        $stripeSubscriptionId = $stripeEvent->data->object->subscription;
+        
+        if ($stripeSubscriptionId){
+          $subscription = $this->findSubscription($stripeSubscriptionId);
+          $stripeSubscription = $this->get('stripe_client')
+            ->findSubscription($stripeSubscriptionId); 
+          
+          $subscriptionHelper->handleSubscriptionPaid($subscription, $stripeSubscription);
+        }
+        break;
+      case 'invoice.payment_failed':
+        $stripeSubscriptionId = $stripeEvent->data->object->id;
+        if ($stripeSubscriptionId){
+          $subscription = $this->findSubscription($stripeSubscriptionId);
+          if($stripeEvent->data->object->attempt_count == 1){
+            
+            $user = $subscription->getUser();
+            
+            $stripeCustomer = $this->get('stripe_client')
+              ->findCustomer($user);
+            $hasCardOnFile = count($stripeCustomer->sources->data) > 0;
+            
+            //todo send an email
+            //use hasCardOnFile to customize this
+          }
+        }
+        break;
+      default:
+        //allow this - we'll have Stripe send us everything
+        //throw new \Exception('Unexpected webhook type form Stripe! '.$stripeEvent->type);
+    }
+    
+    return new Response('Event handled: ' . $stripeEvent->type);  
+  }
+  
+  /**
+   * @param $stripeSubscriptionId
+   * @return \AppBundle\Entity\Subscription
+   * @throws \Exception
+   */
+  private function  findSubscription($stripeSubscriptionId){
+    $subscription = $this->getDoctrine()
+      ->getRepository('AppBundle:Subscription')
+      ->findOneBy([
+          'stripeSubscriptionId' => $stripeSubscriptionId 
+      ]); 
+    
+    if (!$subscription) {
+      throw new \Exception('Somehow we have no subscription id ' . $stripeSubscriptionId);
+    }
+    
+    return $subscription;
+  }
+}
--- src/AppBundle/StripeClient.php
+++ src/AppBundle/StripeClient.php
@@ -3,6 +3,7 @@
 namespace AppBundle;
 
 use AppBundle\Entity\User;
+use AppBundle\Subscription\SubscriptionPlan;
 use Doctrine\ORM\EntityManager;
 
 class StripeClient {
@@ -15,10 +16,15 @@ class StripeClient {
   }
 
   public function createCustomer(User $user, $paymentToken) {
-    $customer = \Stripe\Customer::create([
-      'email' => $user->getEmail(),
-      'source' => $paymentToken,
-    ]);
+    $data = [
+      'email' => $user->getEmail()    
+    ];
+    
+    if($paymentToken){
+      $data['source'] = $paymentToken;  
+    }
+    
+    $customer = \Stripe\Customer::create($data);
     $user->setStripeCustomerId($customer->id);
 
     $this->em->persist($user);
@@ -32,6 +38,8 @@ class StripeClient {
 
     $customer->source = $paymentToken;
     $customer->save();
+    
+    return $customer;
   }
 
   public function createInvoiceItem($amount, User $user, $description) {
@@ -50,9 +58,149 @@ class StripeClient {
 
     if ($payImmediately) {
       // guarantee it charges *right* now
-      $invoice->pay();
+      try {
+        $invoice->pay();
+      } catch(\Stripe\Error\Card $e){
+        $invoice->closed = true;
+        $invoice->save();
+        
+        throw $e;
+      }
     }
 
     return $invoice;
+  }
+  
+  public function createSubscription(User $user, SubscriptionPlan $plan){
+    $subscription = \Stripe\Subscription::create(array(
+      'customer' => $user->getStripeCustomerId(),
+      'plan' => $plan->getPlanId()  
+    ));  
+    
+    return $subscription;
+  }
+  
+  public function cancelSubscription(User $user) {
+    $subscription = \Stripe\Subscription::retrieve(
+      $user->getSubscription()->getStripeSubscriptionId()
+    );  
+    
+    $cancelAtPeriodEnd = true;
+    $currentPeriodEnd = new \DateTime('@'.$subscription->current_period_end);
+    
+    if ($subscription->status == 'past_due'){
+      $cancelAtPeriodEnd = false;
+    } elseif ($currentPeriodEnd < new \DateTime('+1 hour')){
+      $cancelAtPeriodEnd = false;  
+    }
+    
+    $subscription->cancel([
+      'at_period_end' => $cancelAtPeriodEnd
+    ]);
+    
+    return $subscription;
+  }
+  
+  public function reactivateSubscription(User $user) {
+    if(!$user->hasActiveSubscription()){
+      throw new \LogicException('Subcriptions can only be reactivated if the subscription has not actually ended yet');
+    } 
+    
+    $subscription = \Stripe\Subscription::retrieve(
+      $user->getSubscription()->getStripeSubscriptionId()       
+    );
+    //this triggers the refresh of the subscription!
+    $subscription->plan = $user->getSubscription()->getStripePlanId();
+    $subscription->save();
+    
+    return $subscription;
+  }
+  
+  /**
+   * 
+   * @param $eventId
+   * @return \Stripe\Event
+   */
+  public function findEvent($eventId){
+    return \Stripe\Event::retrieve($eventId);
+  }
+  
+  /**
+   * @param $stripeSubscriptionId
+   * @return \Stripe\Subscription
+   */
+  public function findSubscription($stripeSubscriptionId) {
+    return \Stripe\Subscription::retrieve($stripeSubscriptionId);  
+  }
+  
+  public function getUpcomingInvoiceForChangedSubscription(User $user, SubscriptionPlan $newPlan){
+    return \Stripe\Invoice::upcoming([
+      'customer' => $user->getStripeCustomerId(),
+      'subscription' => $user->getSubscription()->getStripeSubscriptionId(),
+      'subscription_plan' =>  $newPlan->getPlanId() 
+    ]);
+  }
+  
+  public function changePlan(User $user, SubscriptionPlan $newPlan){
+    $stripeSubscription = $this->findSubscription($user->getSubscription()->getStripeSubscriptionId());
+    
+    $originalPlanId = $stripeSubscription->plan->id;
+    $currentPeriodStart = $stripeSubscription->current_period_start;
+    
+    $stripeSubscription->plan = $newPlan->getPlanId();
+    $stripeSubscription->save();
+    
+    // if the duration did not change, Stripe will not charge them immediately
+    // but we *do* want them to be charged immediately
+    // if the duration changed, an invoice was already created and paid
+    if ($stripeSubscription->current_period_start == $currentPeriodStart){
+      try {
+      //immediately invoice them
+      $this->createInvoice($user);
+      } catch(\Stripe\Error\Card $e){
+        $stripeSubscription->plan = $originalPlanId;
+        //prevent proration discount/charges from changing back
+        $stripeSubscription->prorate = false;
+        $stripeSubscription->save();
+
+        throw $e;
+      }
+    }
+    
+    return $stripeSubscription;
+  }
+  
+  /*
+   * @param $code
+   * @return \Stripe\Coupon
+   */
+  public function findCoupon($code) {
+    return \Stripe\Coupon::retrieve($code);  
+  }
+  
+  /**
+   * 
+   * @param User $user
+   * @return \Stripe\Invoice[]
+   */
+  public function findCustomer(User $user) {
+    return \Stripe\Customer::retrieve($user->getStripeCustomerId());  
+  }
+  
+  public function findPaidInvoices(User $user){
+    $allInvoices = \Stripe\Invoice::all([
+      'customer' => $user->getStripeCustomerId()    
+    ]);
+    
+    $iterator = $allInvoices->autoPagingIterator();
+    
+    $invoices = array();
+    foreach($iterator as $invoice){
+      if ($invoice->paid){
+        $invoices[] = $invoice;
+      }
+    }
+    
+    return $invoices;
   }
 }
