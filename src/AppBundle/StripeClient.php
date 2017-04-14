421140a196c184cbddb269db61f84f0b21e74988
petrero <petrero@gmail.com>
2017-04-14 21:12:39 +0300

    Fetch all Paid Invoices
    Open up StripeClient. At the bottom, add a new public function findPaidInvoices() with a User argument.
    Here's the idea: we'll use Stripe's API to find all Invoices for a customer, but then filter those to only return invoices that were paid. That will remove some garbage invoices the user shouldn't see: like invoices for payments that failed and then were closed immediately.
    
    Start with: {dollar_sign}allInvoices = \Stripe\Invoice::all() and pass that an array with a customer key set to {dollar_sign}user->getStripeCustomerId(). Next - and this will look a little weird at first - create an {dollar_sign}iterator variable set to {dollar_sign}allInvoices->autoPagingIterator().
    This is actually really cool: if the user has a lot of invoices, then Stripe will paginate your results. But with the iterator, it will automatically make new API calls behind-the-scenes, allowing us to loop over every invoice, no matter how many there are.
    
    Let's do that: start with {dollar_sign}invoices = array(). Then foreach over {dollar_sign}iterator as {dollar_sign}invoice. Very simply, we want to know if this invoice is paid. If {dollar_sign}invoice->paid, then add this to the {dollar_sign}invoices array. Finally, return those paid {dollar_sign}invoices at the bottom

--- src/AppBundle/StripeClient.php
+++ src/AppBundle/StripeClient.php
@@ -181,4 +181,21 @@ class StripeClient {
   public function findCustomer(User $user) {
     return \Stripe\Customer::retrieve($user->getStripeCustomerId());  
   }
+  
+  public function findPaidInvoices(User $user){
+    $allInvoices = \Stripe\Invoice::all([
+      'customer' => $user->getStripeCustomerId()    
+    ]);
+    
+    $iterator = $allInvoices->autoPagingIterator();
+    
+    $invoices = array();
+    foreach($iterator as $invoice){
+      if ($invoice->paid){
+        $invoices[] = $invoice;
+      }
+    }
+    
+    return $invoices;
+  }
 }
